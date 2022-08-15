129fbf6a7edd1a2e343ed9dda2d2fe16aba5b844
NewWep <98640331+junecheolpark@users.noreply.github.com>
2022-08-15 17:28:54 +0900

    문자열 90%

--- baekjoon/src/f문자열/f09_크로아티아알파벳.java
+++ baekjoon/src/f문자열/f09_크로아티아알파벳.java
@@ -1,40 +1,87 @@
 package f문자열;
 
 
-/*/
-평균은 넘겠지
 
-문제
-대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
-
-입력
-첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
-
-둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다. 
-점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
-
-출력
-각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
-
-예제 입력1 5
-		5 50 50 70 80 100
-		7 100 95 90 80 70 60 50
-		3 70 90 80
-		3 70 90 81
-		9 100 99 98 97 96 95 94 93 91
-
-예제 출력1 40.000%
-		57.143%
-		33.333%
-		66.667%
-		55.556%
-/**/
 import java.util.Scanner;
+
 public class f09_크로아티아알파벳 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-			
+    
+		Scanner in = new Scanner(System.in);
+        
+		String str = in.nextLine();
+        
+		int count = 0;
+ 
+		for (int i = 0; i < str.length(); i++) {
+ 
+			char ch = str.charAt(i);
+ 
+			if(ch == 'c') {			// 만약 ch 가 c 라면?
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {		//만약 ch 다음 문자가 '=' 이라면?
+						// i+1 까지가 하나의 문자이므로 다음 문자를 건너 뛰기 위해 1 증가
+						i++;		
+					}
+					else if(str.charAt(i + 1) == '-') {
+						i++;
+					}
+				}
+			}
+		    
+			else if(ch == 'd') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'z') {
+						if(i < str.length() - 2) {
+							if(str.charAt(i + 2) == '=') {	// dz= 일 경우
+								i += 2;
+							}
+						}
+					}
+		        
+					else if(str.charAt(i + 1) == '-') {	// d- 일 경우
+						i++;
+					}
+				}
+			}
+		    
+			else if(ch == 'l') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'j') {	// lj 일 경우
+						i++;
+					}
+				}
+			}
+		    
+			else if(ch == 'n') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'j') {	// nj 일 경우
+						i++;
+					}
+				}
+			}
+ 
+			else if(ch == 's') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {	// s= 일 경우
+						i++;
+					}
+				}
+		    }
+ 
+			else if(ch == 'z') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {	// z= 일 경우
+						i++;
+					}
+				}
+			}
+		    
+			count++;
+ 
 		}
+ 
+		System.out.println(count);
 	}
+}
 
