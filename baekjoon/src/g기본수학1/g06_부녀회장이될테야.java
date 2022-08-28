d1d5c7ce37bcd20146a71bf8c320f2c2d7ad0acc
junecheolpark <skswnscjf2@naver.com>
2022-08-29 03:53:08 +0900

    80%

--- baekjoon/src/g기본수학1/g06_부녀회장이될테야.java
+++ baekjoon/src/g기본수학1/g06_부녀회장이될테야.java
@@ -1,6 +1,5 @@
 package g기본수학1;
 
-
 /*/
 부녀회장이 될테야
 
@@ -27,11 +26,33 @@ package g기본수학1;
 		10
 /**/
 import java.util.Scanner;
+
 public class g06_부녀회장이될테야 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-			
+		Scanner in = new Scanner(System.in);
+
+		// 아파트 생성
+		int[][] APT = new int[15][15];
+
+		for (int i = 0; i < 15; i++) {
+			APT[i][1] = 1; // i층 1호
+			APT[0][i] = i; // 0층 i호
+		}
+
+		for (int i = 1; i < 15; i++) { // 1층부터 14층까지
+
+			for (int j = 2; j < 15; j++) { // 2호부터 14호까지
+				APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
+			}
+		}
+
+		// 테스트 부분
+		int T = in.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			int k = in.nextInt();
+			int n = in.nextInt();
+			System.out.println(APT[k][n]);
 		}
 	}
-
+}
