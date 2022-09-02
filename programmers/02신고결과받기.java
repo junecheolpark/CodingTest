50e8e3137ac757a1ec119c71f686345cda497a41
junecheolpark <skswnscjf2@naver.com>
2022-09-02 23:51:34 +0900

    ready

---
+++ programmers/02신고결과받기.java
@@ -0,0 +1,16 @@
+
+
+/*/
+신고 결과 받기
+
+
+/**/
+import java.util.Scanner;
+public class 02신고결과받기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+			
+		}
+	}
+
