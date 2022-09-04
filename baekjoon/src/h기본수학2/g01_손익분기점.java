0f5054707c942deed1a9445f7e80407ecbcf25fe
junecheolpark <skswnscjf2@naver.com>
2022-09-04 23:50:51 +0900

    기본수학2

--- baekjoon/src/h기본수학2/g01_손익분기점.java
+++ baekjoon/src/h기본수학2/g01_손익분기점.java
@@ -2,44 +2,54 @@ package h기본수학2;
 
 
 /*/
-손익분기점
+소수 찾기
 
 문제
-월드전자는 노트북을 제조하고 판매하는 회사이다. 노트북 판매 대수에 상관없이 매년 임대료, 재산세, 보험료, 급여 등 A만원의 고정 비용이 들며, 한 대의 노트북을 생산하는 데에는 재료비와 인건비 등 총 B만원의 가변 비용이 든다고 한다.
-
-예를 들어 A=1,000, B=70이라고 하자. 이 경우 노트북을 한 대 생산하는 데는 총 1,070만원이 들며, 열 대 생산하는 데는 총 1,700만원이 든다.
-
-노트북 가격이 C만원으로 책정되었다고 한다. 일반적으로 생산 대수를 늘려 가다 보면 어느 순간 총 수입(판매비용)이 총 비용(=고정비용+가변비용)보다 많아지게 된다. 최초로 총 수입이 총 비용보다 많아져 이익이 발생하는 지점을 손익분기점(BREAK-EVEN POINT)이라고 한다.
-
-A, B, C가 주어졌을 때, 손익분기점을 구하는 프로그램을 작성하시오.
-
+주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
 입력
-첫째 줄에 A, B, C가 빈 칸을 사이에 두고 순서대로 주어진다. A, B, C는 21억 이하의 자연수이다.
+첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다
 
 출력
-첫 번째 줄에 손익분기점 즉 최초로 이익이 발생하는 판매량을 출력한다. 손익분기점이 존재하지 않으면 -1을 출력한다.
+주어진 수들 중 소수의 개수를 출력한다.
 
-예제 입력1 1000 70 170
+예제 입력1 4
+		1 3 5 7
 
-예제 출력1 11
+예제 출력1 3
 /**/
 import java.util.Scanner;
 
 public class g01_손익분기점 {
 	public static void main(String[] args) {
+		
 		Scanner in = new Scanner(System.in);
- 
- 
-		int A = in.nextInt();	// 불변 비용
-		int B = in.nextInt();	// 가변 비용
-		int C = in.nextInt(); 	// 상품 가격
-        
-		if (C <= B) {
-			System.out.println("-1");
-		} 
-		else {
-			System.out.println((A/(C-B))+1);
+		
+		int N = in.nextInt();
+		
+		int count = 0;
+		
+		for(int i = 0; i < N; i++) {
+			
+			// 소수인경우 true, 아닌경우 false   
+			boolean isPrime = true;
+			
+			int num = in.nextInt();
+			
+			if(num == 1) {	// 1 인경우 다음 반복문으로
+				continue;
+			}
+			for(int j = 2; j <= Math.sqrt(num); j++) {
+				if(num % j == 0) {
+					isPrime = false;	// 소수가 아니므로 false 로 바꿔줌
+					break;
+				}
+			}
+			if(isPrime) {	// 소수인경우 count 값 1 증가
+				count++;
+			}
 		}
+		System.out.println(count);
 	}
+ 
 }
 
