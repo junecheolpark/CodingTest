e36872293ff3ddce1d59c7e77efa06f2612c69d7
junecheolpark <skswnscjf2@naver.com>
2022-09-17 22:24:07 +0900

    programmers 25

--- programmers_level1/src/a24_문자열내림차순으로배치하기.java
+++ programmers_level1/src/a24_문자열내림차순으로배치하기.java
@@ -15,7 +15,7 @@ public class a24_문자열내림차순으로배치하기 {
 class Solution24 {
 	public String solution(String s) {
 		String[] arr = s.split(""); // String배열로 변환
-		Arrays.sort(arr, Collections.reverseOrder());
-		return String.join("", arr);
+		Arrays.sort(arr, Collections.reverseOrder()); // 내림차순 정렬
+		return String.join("", arr); // 배열을 ""로 연결해줘 리턴
 	}
 }
