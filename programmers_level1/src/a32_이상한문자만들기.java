d283cdb1d8a6a4e7ff0b56b191dafeac3b91597e
junecheolpark <skswnscjf2@naver.com>
2022-09-22 00:40:19 +0900

    prm 32

--- programmers_level1/src/a32_이상한문자만들기.java
+++ programmers_level1/src/a32_이상한문자만들기.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class a32_이상한문자만들기 {
@@ -11,16 +10,42 @@ public class a32_이상한문자만들기 {
 }
 
 class Solution32 {
-	public String solution(String s) {
-		char[] ch = s.toCharArray();
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < s.length(); i++) {
-			if (i % 2 == 0) {
-				sb.append(String.valueOf(ch[i]).toUpperCase());
-			} else {
-				sb.append(ch[i]);
-			}
-		}
-		return sb.toString();
-	}
-}
+    public String solution(String s) {
+    	/*/ 안되는 이유가 뭘까
+        char[] ch = {};
+        String[] arr = s.split(" ");
+        
+        for(int i = 0; i<arr.length; i++) {
+        	StringBuilder sb = new StringBuilder();
+        	ch = arr[i].toCharArray();
+        	 for(int j = 0; j<arr[i].length(); j++) {
+        		 if(j%2==0) {
+        			 sb.append(String.valueOf(ch[j]).toUpperCase());
+        		 }else {
+        			 sb.append(String.valueOf(ch[j]).toLowerCase());
+        		 }
+        		
+        	 }
+        	 arr[i] = sb.toString();
+        }
+        return String.join(" ",arr);
+        /**/
+    	
+            String[] str = s.split(""); // 각각 배열로 저장
+            StringBuilder sb = new StringBuilder();
+            int idx = 0;
+            for(int i=0; i<str.length; i++) {
+                if(str[i].equals(" ")) { // 공백이라면 인덱스 초기화
+                    sb.append(str[i]);
+                    idx = 0;
+                } else if(idx % 2 == 0) { // 인덱스가 짝수라면 대문자변경후 index++
+                    sb.append(str[i].toUpperCase());
+                    idx++;
+                } else if(idx % 2 != 0) { // 인덱스가 홀수라면 대문자 일수도 있으니 소문자변경후 index++
+                    sb.append(str[i].toLowerCase());
+                    idx++;
+                }
+                
+            } 
+            return sb.toString(); // 
+        }
