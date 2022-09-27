92a526b505997607380cd0d1c2ad1f6d2ec687d1
junecheolpark <skswnscjf2@naver.com>
2022-09-28 00:06:23 +0900

    prm 34

--- programmers_level1/src/a34_시저암호.java
+++ programmers_level1/src/a34_시저암호.java
@@ -6,33 +6,28 @@ public class a34_시저암호 {
 		Scanner sc = new Scanner(System.in);
 		String s = "a B z";
 		int n = 4;
-				
+
 		String answer = new Solution34().solution(s, n);
 		System.out.println(answer);
 	}
 }
 
 class Solution34 {
-    public String solution(String s, int n) {
-        char ch[] = s.toCharArray();
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i<s.length(); i++) {
-        	System.out.println(1);
-        	 for(int j = 0 ; j<n; j++) {
-        		 System.out.println((char)(ch[i]-25));
-        		 if(ch[i] == ' ') {
-        			 sb.append(ch[i]);
-        		 }else if(ch[i]!='z' && ch[i]!='Z') {
-        			 sb.append((char)(ch[i] + 1));
-        		 }else {
-        			 sb.append((char)(ch[i] -25));
-        		 }
-        		 
-        		 
-        		
-        	 }
-        	
-        }
-        return String.valueOf(sb.toString());
-    }
+	public String solution(String s, int n) {
+		 String answer = "";
+         
+         for(int i=0; i<s.length(); i++) {
+             char ch = s.charAt(i);
+             if(Character.isLowerCase(ch)) { //소문자
+                 ch = (char) ((ch - 'a' + n) % 26 + 'a');
+             } else if(Character.isUpperCase(ch)) { //대문자
+                 ch = (char) ((ch - 'A' + n) % 26 + 'A');
+             }
+             
+             answer += ch;
+         }
+             
+         
+         return answer;
+	}
 }
