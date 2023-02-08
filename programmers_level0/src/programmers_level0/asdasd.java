2e8b583931820a3fdf211951ef4c0425d8330437
junecheolpark <skswnscjf2@naver.com>
2023-02-08 18:05:39 +0900

    짝수의합

---
+++ programmers_level0/src/programmers_level0/asdasd.java
@@ -0,0 +1,22 @@
+package programmers_level0;
+
+import java.util.Scanner;
+
+public class asdasd {
+	public void main(String[] arg) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int answer = solution1(n);
+		System.out.println(answer);
+	}
+
+	public int solution1(int n) {
+		int answer = 0;
+		for (int i = 0; i <= n; i += 2) {
+
+			answer += i;
+
+		}
+		return answer;
+	}
+}
