f20c41045e254339e839509606dcedcafc67f267
junecheolpark <skswnscjf2@naver.com>
2023-02-09 18:12:25 +0900

    문자열뒤집기

---
+++ programmers_level0/src/programmers_level0/_a04_문자열뒤집기.java
@@ -0,0 +1,17 @@
+package programmers_level0;
+
+import java.util.Scanner;
+
+public class _a04_문자열뒤집기 {
+	public static void main(String[] arg) {
+		Scanner sc = new Scanner(System.in);
+		String s = "jaron";
+		String answer = solution1(s);
+		System.out.println(answer);
+	}
+
+	public static String solution1(String my_string) {
+		StringBuffer sb = new StringBuffer(my_string);
+        return sb.reverse().toString();
+	}
+}
