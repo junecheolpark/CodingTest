84968a5ca3de0232681215b3d98b59a55c0f732f
junecheolpark <skswnscjf2@naver.com>
2023-02-14 09:25:53 +0900

    편지

---
+++ programmers_level0/src/programmers_level0/_a22_편지.java
@@ -0,0 +1,15 @@
+package programmers_level0;
+
+import java.util.*;
+
+public class _a22_ÆíÁö {
+	public static void main(String[] arg) {
+		// int[] arr = { 1, 2, 3, 4, 5 };
+		// String str = "hello";
+		System.out.println((solution("happy birthday!")));
+	}
+
+	public static int solution(String message) {
+		return message.length() * 2;
+	}
+}
