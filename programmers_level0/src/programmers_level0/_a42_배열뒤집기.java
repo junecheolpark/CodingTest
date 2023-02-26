13e1d153c83cfb1cddc31d02e4b2751bfce524b9
junecheolpark <skswnscjf2@naver.com>
2023-02-26 18:48:03 +0900

    배열뒤집기

--- programmers_level0/src/programmers_level0/_a42_배열뒤집기.java
+++ programmers_level0/src/programmers_level0/_a42_배열뒤집기.java
@@ -4,22 +4,34 @@ import java.util.Arrays;
 
 public class _a42_배열뒤집기 {
 	public static void main(String[] arg) {
-		int[] arr3 = { 1, 8, 3 };
-		int[] arr2 = { 9, 10, 11, 8 };
+		int[] arr2 = { 4, 455, 6, 4, -1, 45, 6 };
+		int[] arr3 = { 1, 2, 3 };
 		// String str = "hello";
 		// String[] sarr = { "a", "b", "c" };
-		System.out.println(Arrays.toString(solution(arr2)));
-		System.out.println(Arrays.toString(solution(arr3)));
+		System.out.println(Arrays.toString(solution(arr2, "left")));
+		// System.out.println(Arrays.toString(solution(arr3, "right")));
 	}
 
-	public static int[] solution(int[] array) {
-		int len = array.length-1;
-		System.out.println(len);
+	public static int[] solution(int[] numbers, String direction) {
+		int len = numbers.length;
 		int[] answer = new int[len];
-		Arrays.toString(answer);
-		for (int i = len; i > 0; i--) {
-			System.out.println(array[i]);
-			answer[len - i] = array[i];
+		if (direction.equals("right")) {
+			for (int i = 0; i < len; i++) {
+				if (i == 0) {
+					answer[i] = numbers[len - 1];
+				} else {
+					answer[i] = numbers[i - 1];
+				}
+
+			}
+		} else if (direction.equals("left")) {
+			for (int i = 0; i < len; i++) {
+				if (i == len - 1) {
+					answer[i] = numbers[0];
+				} else {
+					answer[i] = numbers[i + 1];
+				}
+			}
 		}
 		return (answer);
 
