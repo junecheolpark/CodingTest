55dafaa30b0a10e3001bbb6fef3107b40b3b448c
junecheolpark <skswnscjf2@naver.com>
2023-03-06 13:31:39 +0900

    2차원으로만들기

--- programmers_level0/src/programmers_level0/_a52_a로b만들기.java
+++ programmers_level0/src/programmers_level0/_a52_a로b만들기.java
@@ -1,28 +1,26 @@
 package programmers_level0;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class _a52_a로b만들기 {
 	public static void main(String[] arg) {
 		// int[] arr2 = { 4, 455, 6, 4, -1, 45, 6 };
-		// int[] arr3 = { 1, 2, 3 };
+		int[] arr3 = { 1, 2, 3, 4, 5, 6, 7, 8 };
 		// String str = "hello";
 		// String[] sarr = { "a", "b", "c" };
-		System.out.println((solution("olleh", "hello")));
-		System.out.println((solution("allpe", "apple")));
+		System.out.println(Arrays.toString(solution(arr3, 2)));
+		// System.out.println((solution(7)));
 	}
 
-	public static int solution(String before, String after) {
-
-		int answer = 0;
-		char[] b_ch = before.toCharArray();
-		char[] a_ch = after.toCharArray();
-
-		Arrays.sort(b_ch);
-		Arrays.sort(a_ch);
-
-		return new String(b_ch).equals(new String(a_ch)) ? 1 : 0;
+	public static int[][] solution(int[] num_list, int n) {
+		int[][] answer = new int[num_list.length / n][n];
+		int count = 0;
+		for (int i = 0; i < answer.length; i++) {
+			for (int j = 0; j < answer[i].length; j++) {
+				answer[i][j] = num_list[count];
+				count++;
+			}
+		}
+		return answer;
 	}
 }
---
+++ programmers_level0/src/programmers_level0/_a54_2차원으로만들기.java
@@ -0,0 +1,24 @@
+package programmers_level0;
+
+import java.util.Arrays;
+
+public class _a54_2차원으로만들기 {
+	public static void main(String[] arg) {
+		// int[] arr2 = { 4, 455, 6, 4, -1, 45, 6 };
+		// int[] arr3 = { 1, 2, 3 };
+		// String str = "hello";
+		// String[] sarr = { "a", "b", "c" };
+		System.out.println((solution("olleh", "hello")));
+		System.out.println((solution("allpe", "apple")));
+	}
+
+	public static int solution(String before, String after) {
+		char[] b_ch = before.toCharArray();
+		char[] a_ch = after.toCharArray();
+
+		Arrays.sort(b_ch);
+		Arrays.sort(a_ch);
+
+		return new String(b_ch).equals(new String(a_ch)) ? 1 : 0;
+	}
+}
