7a5ba9d93c6a42c18395e4559d65f036052860c3
junecheolpark <skswnscjf2@naver.com>
2023-03-08 18:01:21 +0900

    test

---
+++ programmers_level0/src/programmers_level0/_a58_한번만등장하는문자.java
@@ -0,0 +1,39 @@
+package programmers_level0;
+
+import java.util.Arrays;
+
+public class _a58_한번만등장하는문자 {
+	public static void main(String[] arg) {
+		// int[] arr2 = { 4, 455, 6, 4, -1, 45, 6 };
+		// int[] arr1 = { 3, 76, 24 };
+		// int[] arr2 = { 1, 2, 3, 4, 5, 6, 7 };
+		// int[] arr3 = { 30, 10, 23, 6, 100 };
+		// String str = "hello";
+		// String[] sarr = { "a", "b", "c" };
+		System.out.println((solution("abcabcadc")));
+		System.out.println((solution("abdc")));
+		System.out.println((solution("hello")));
+	}
+
+	public static String solution(String s) {
+		StringBuilder sb = new StringBuilder();
+		char[] arr = s.toCharArray();
+		int cnt = 0;
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			if (i == arr.length - 1) {
+				cnt = arr[i] == arr[i - 1] ? +1 : +0;
+				if (cnt == 0) sb.append(arr[i]);
+			} else {
+				if (arr[i] == arr[i + 1]) {
+					cnt++;
+					continue;
+				} else {
+					if (cnt == 0) sb.append(arr[i]);
+					cnt = 0;
+				}
+			}
+		}
+		return sb.toString();
+	}
+}
