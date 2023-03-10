0ae30af04dbbbf5e3ec5fcfb3b2ca9c0528e80dd
junecheolpark <skswnscjf2@naver.com>
2023-03-10 15:31:38 +0900

    한번만들어있는 문자

--- programmers_level0/src/programmers_level0/_a58_한번만등장하는문자.java
+++ programmers_level0/src/programmers_level0/_a58_한번만등장하는문자.java
@@ -17,14 +17,15 @@ public class _a58_한번만등장하는문자 {
 
 	public static String solution(String s) {
 		StringBuilder sb = new StringBuilder();
+		if(s.length() == 1) return s;
 		char[] arr = s.toCharArray();
 		int cnt = 0;
 		Arrays.sort(arr);
 		for (int i = 0; i < arr.length; i++) {
-			if (i == arr.length - 1) {
+			if (i == arr.length - 1) { // 마지막 인덱스라면 그전과 비교
 				cnt = arr[i] == arr[i - 1] ? +1 : +0;
 				if (cnt == 0) sb.append(arr[i]);
-			} else {
+			} else { // 0~ 마지막인덱스 전 -> cnt가 0이면 중복x 
 				if (arr[i] == arr[i + 1]) {
 					cnt++;
 					continue;
@@ -37,3 +38,20 @@ public class _a58_한번만등장하는문자 {
 		return sb.toString();
 	}
 }
+
+/*
+int[] alpha = new int[26]; // a~z
+for(char c : s.toCharArray()){
+    alpha[c - 'a']++;
+}
+
+StringBuilder answer = new StringBuilder();
+for(int i = 0; i < 26; i++){
+    if(alpha[i] == 1){
+        answer.append((char)(i + 'a'));
+    }
+}
+return answer.toString();
+}
+}
+*/
