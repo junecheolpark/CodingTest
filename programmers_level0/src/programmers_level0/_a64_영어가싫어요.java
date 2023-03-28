8a43379e3b74b54697da1683129d850709b99ef3
junecheolpark <skswnscjf2@naver.com>
2023-03-28 17:47:35 +0900

    영어가싫어요

--- programmers_level0/src/programmers_level0/_a64_영어가싫어요.java
+++ programmers_level0/src/programmers_level0/_a64_영어가싫어요.java
@@ -5,11 +5,16 @@ import java.util.Arrays;
 public class _a64_영어가싫어요 {
 	public static void main(String[] arg) {
 		// int[] arr1 = { 1, 2, 3, 4 };
-		System.out.println((solution("1 2 Z 3")));
+		System.out.println((solution("onetwothreefourfivesixseveneightnine")));
 	}
 
 	public static long solution(String numbers) {
-		long answer = 0;
-		return answer;
+		String[] numbers_arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < numbers_arr.length; i++) {
+            numbers = numbers.replace(numbers_arr[i], String.valueOf(i));
+        }
+
+        return Long.parseLong(numbers);
 	}
 }
