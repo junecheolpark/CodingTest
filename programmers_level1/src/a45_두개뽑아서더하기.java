4f2255ae577d2d09986c6e0e604b97ed03901e5f
junecheolpark <skswnscjf2@naver.com>
2023-07-03 18:39:11 +0900

    save

---
+++ programmers_level1/src/a45_두개뽑아서더하기.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/68644
+
+public class a45_두개뽑아서더하기 {
+	public static void main(String[] arg) {
+		int[] array = { 2, 1, 3, 4, 1 };
+		int[] Array = solution(array);
+		for (int i = 0; i < Array.length; i++) {
+			System.out.println(Array[i]);
+		}
+
+	}
+
+	public static int[] solution(int[] numbers) {
+		HashSet<Integer> set = new HashSet<>();
+		Arrays.sort(numbers);
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i; j < numbers.length - 1; j++) {
+//				System.out.print(j + " : " + (numbers[j] + numbers[j + 1]) + ", ");
+				set.add(numbers[i] + numbers[j + 1]);
+			}
+//			System.out.println();
+		}
+		int[] answer = new int[set.size()];
+		int index = 0;
+		for (int num : set) {
+			answer[index++] = num;
+		}
+		return answer;
+	}
+}
