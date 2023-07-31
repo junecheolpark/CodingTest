1ef30c7a4af8c9068b8bac1a7d6083977af226d8
junecheolpark <skswnscjf2@naver.com>
2023-07-31 16:15:09 +0900

    소수만들기

---
+++ programmers_level1/src/a54_소수만들기.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12977
+
+public class a54_소수만들기 {
+	public static void main(String[] arg) {
+		int[] arr = { 1, 2, 3, 4 };
+		int[] arr2 = { 1, 2, 7, 6, 4 };
+		System.out.println(solution(arr2));
+
+	}
+
+	public static boolean minority(int number) {
+	    for(int i = 2; i <= number/2; i++) {
+	        if(number % i == 0) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+	
+	public static int solution(int[] nums) {
+		int length = nums.length;
+		int cnt = 0;
+
+		for (int i = 0; i < length - 2; i++) {
+			for (int j = i + 1; j < length - 1; j++) {
+				for (int k = j + 1; k < length; k++) {
+					if (minority(nums[i] + nums[j] + nums[k]))
+						cnt++;
+				}
+			}
+		}
+
+		return cnt;
+	}
+}
