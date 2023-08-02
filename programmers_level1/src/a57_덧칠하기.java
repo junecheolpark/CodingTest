c27c00395ca4cad87ce0595a1be4f6b53efd5c3d
junecheolpark <skswnscjf2@naver.com>
2023-08-02 23:33:58 +0900

    setting

---
+++ programmers_level1/src/a57_덧칠하기.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/161989
+
+public class a57_µ¡Ä¥ÇÏ±â {
+	public static void main(String[] arg) {
+		int[] arr = { 2, 3, 6 };
+		int[] arr2 = { 4, 4, 4, 4, 4 };
+//		int[] answer = solution(5, arr);
+		int answer2 = solution(8, 4, arr2);
+		System.out.println(answer2);
+	}
+
+	public static int solution(int n, int m, int[] section) {
+		int answer = 0;
+		return answer;
+	}
+}
