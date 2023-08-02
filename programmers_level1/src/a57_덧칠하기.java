d9ae4208a56568b87b2f0bbaa3161d2b65908760
junecheolpark <skswnscjf2@naver.com>
2023-08-03 00:15:08 +0900

    덧칠하기

--- programmers_level1/src/a57_덧칠하기.java
+++ programmers_level1/src/a57_덧칠하기.java
@@ -6,14 +6,24 @@ import java.util.Comparator;
 public class a57_µ¡Ä¥ÇÏ±â {
 	public static void main(String[] arg) {
 		int[] arr = { 2, 3, 6 };
-		int[] arr2 = { 4, 4, 4, 4, 4 };
+		int[] arr2 = { 1, 3 };
+		int[] arr3 = { 1, 2, 3, 4 };
 //		int[] answer = solution(5, arr);
-		int answer2 = solution(8, 4, arr2);
-		System.out.println(answer2);
+		System.out.println(solution(8, 4, arr));
+		System.out.println(solution(5, 4, arr2));
+		System.out.println(solution(4, 1, arr3));
 	}
 
 	public static int solution(int n, int m, int[] section) {
-		int answer = 0;
-		return answer;
+		int num = section[0];
+		int cnt = 1;
+
+		for (int i = 0; i < section.length; i++) {
+			if (num + m - 1 < section[i]) {
+				cnt++;
+				num = section[i];
+			}
+		}
+		return cnt;
 	}
 }
