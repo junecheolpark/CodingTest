c864c8ecda2f4a1d62052ae0a77c3b3c149e34f9
junecheolpark <skswnscjf2@naver.com>
2023-09-07 18:31:20 +0900

    이진변환 반복하기

--- programmers_level2/src/programmers_level2/a03_이진변환반복하기.java
+++ programmers_level2/src/programmers_level2/a03_이진변환반복하기.java
@@ -5,30 +5,37 @@ import java.util.Arrays;
 //https://school.programmers.co.kr/learn/courses/30/lessons/70129
 public class a03_이진변환반복하기 {
 	public static void main(String[] args) {
-		
+
 		System.out.println(Arrays.toString(solution("110010101001")));
+		System.out.println(Arrays.toString(solution("01110")));
+		System.out.println(Arrays.toString(solution("1111111")));
 	}
 
 	public static int[] solution(String s) {
-        int[] answer = {};
-        int cnt = 0;
-        int zeroCnt = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '0') {
-            	zeroCnt++;
-            }
-        }
-        System.out.println(zeroCnt);
-        
-        System.out.println(s.replaceAll("0", ""));
-        return answer;
-    }
+		int[] answer = {1,0};
+		int sLength = 0;
+
+		answer[1] += zeroRemove(s);
+		sLength = s.replaceAll("0", "").length();
+		String oneStr = Integer.toBinaryString(sLength); // 1길이값 2진수로 변환
+
+		while (sLength > 1) { // 문자열의 길이값이 1일때까지 반복
+			answer[1] += zeroRemove(oneStr);
+			oneStr = Integer.toBinaryString((oneStr.replaceAll("0", "").length()));
+			sLength = oneStr.length();
+			answer[0]++;
+		}
+		return answer;
+	}
+
+	public static int zeroRemove(String str) { // 0 개수 확인해서 리턴
+		int cnt = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '0') {
+				cnt++;
+			}
+		}
+		return cnt;
+	}
 
 }
-/*
-String binaryString = "1101"; // 2진수
-int decimal = Integer.parseInt(binaryString, 2); // 2진수를 10진수로 변환
-int decimalNumber = 13; // 10진수
-String binaryString = Integer.toBinaryString(decimalNumber); // 10진수를 2진수로 변환
-*/
