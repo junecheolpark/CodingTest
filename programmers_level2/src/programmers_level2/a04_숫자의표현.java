41a92d0c35494b20821384a089b9acd31883be5d
junecheolpark <skswnscjf2@naver.com>
2023-09-08 13:32:57 +0900

    숫자의표현 setting

---
+++ programmers_level2/src/programmers_level2/a04_숫자의표현.java
@@ -0,0 +1,15 @@
+package programmers_level2;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12924
+public class a04_숫자의표현 {
+	public static void main(String[] args) {
+		System.out.println((solution(15)));
+	}
+
+	public static int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+
+
+}
