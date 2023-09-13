43c475e54c97b615fa4e1ee7c9fca1e77c5dc731
junecheolpark <skswnscjf2@naver.com>
2023-09-13 14:02:37 +0900

    숫자의 표현

--- programmers_level2/src/programmers_level2/a04_숫자의표현.java
+++ programmers_level2/src/programmers_level2/a04_숫자의표현.java
@@ -7,9 +7,24 @@ public class a04_숫자의표현 {
 	}
 
 	public static int solution(int n) {
-        int answer = 0;
-        return answer;
-    }
-
-
+		int answer = 0;
+		int sum = 0;
+		int j = 0;
+		for (int i = 1; i <= n; i++) {
+			sum = i;
+			j = i;
+			while (true) {
+				System.out.println(sum);
+				if (sum == n) {
+					answer++;
+				} else if (sum > n) {
+					break;
+				}
+				j += 1;
+				sum += j;
+			}
+		}
+		return answer;
+	}
+	
 }
