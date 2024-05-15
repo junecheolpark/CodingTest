2416227246cfddbbb4596df5708a7b122e4a18e4
skswnscjf2@naver.com <skswnscjf2@naver.com>
2024-05-15 23:39:32 +0900

    이전 최소공배수 최대공약수 수정

--- programmers_level0/src/programmers_level0/최소공약수와배수.java
+++ programmers_level0/src/programmers_level0/최소공약수와배수.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class 최소공약수와배수 {
 	public static void main(String[] args) {
 
-		int num1 = 4;
-		int num2 = 3;
+		int num1 = 24;
+		int num2 = 14;
 
 		int gcd = getGCD(num1, num2);
 		System.out.println("최대공약수 : " + gcd);
