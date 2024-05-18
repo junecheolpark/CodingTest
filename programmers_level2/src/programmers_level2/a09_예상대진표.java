b2641bdcd3e8ca26438a41ccf178d882efd4cf3c
skswnscjf2@naver.com <skswnscjf2@naver.com>
2024-05-19 00:32:10 +0900

    예상대진표

--- programmers_level2/src/programmers_level2/a09_예상대진표.java
+++ programmers_level2/src/programmers_level2/a09_예상대진표.java
@@ -5,35 +5,19 @@ import java.util.Arrays;
 //https://school.programmers.co.kr/learn/courses/30/lessons/12985
 public class a09_예상대진표 {
 	public static void main(String[] args) {
-		int[] arr = {2, 6, 8, 14};
-        System.out.println(solution(arr)); // 출력: 168
+		System.out.println(solution(8, 4, 7)); // 출력: 3
 	}
 
-	// 최대공약수 계산 메서드
-    public static int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-
-	// 최소공배수 계산 메서드
-	public static int lcm(int a, int b) {
-//      return a * (b / gcd(a, b));
-		return (a * b) / gcd(a, b);
-	}
-    
-    // 배열의 모든 수의 최소공배수 계산 메서드
-    public static int solution(int[] arr) {
-    	/**
-    	int answer = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            answer = lcm(answer, arr[i]);
-        }
-        */
-        return Arrays.stream(arr).reduce(1, (a, b) -> lcm(a, b));
+    public static int solution(int N, int A, int B) {
+    	int answer = 0;
+    	
+		while (A != B) {
+			A -= A / 2;
+			B -= B / 2;
+			answer++;
+		}
+    	
+        return answer;
     }
 
 }
