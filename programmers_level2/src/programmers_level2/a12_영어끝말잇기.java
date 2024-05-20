de009f44a88ca2ddecb36dfd82fe5fd844459f8a
junecheolpark <skswnscjf2@naver.com>
2024-05-20 17:31:45 +0900

    12. ready

---
+++ programmers_level2/src/programmers_level2/a12_영어끝말잇기.java
@@ -0,0 +1,21 @@
+package programmers_level2;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12981
+public class a12_영어끝말잇기 {
+	public static void main(String[] args) {
+		String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"} ;
+		int n = 3; 
+
+		System.out.println(solution(n, words)); // 출력: 5
+	}
+	
+	public static int[] solution(int n, String[] words) {
+	int[] answer = {};
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
+    System.out.println("Hello Java");
+
+    return answer;
+	}
+
+}
