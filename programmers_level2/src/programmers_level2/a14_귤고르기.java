30eda6b476898123816fba3e2b12db09c79fba3f
junecheolpark <skswnscjf2@naver.com>
2024-05-24 12:27:20 +0900

    귤고르기

--- programmers_level2/src/programmers_level2/a14_귤고르기.java
+++ programmers_level2/src/programmers_level2/a14_귤고르기.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/12980
+//https://school.programmers.co.kr/learn/courses/30/lessons/138476
 public class a14_귤고르기 {
 	public static void main(String[] args) {
 
@@ -13,28 +13,28 @@ public class a14_귤고르기 {
 	}
 
 	public static int solution(int k, int[] tangerine) {
-		int answer = 0;
-		int kCnt = 0;
-		Map<Integer, Integer> map = new HashMap<>();
-		// getOrDefault 키가 없으면 지정된값 반환
-		for (int num : tangerine) {
-			map.put(num, map.getOrDefault(num, 0) + 1);
-		}
-		int[] list = new int[map.size()];
-		int index = 0;
-		for (Integer key : map.keySet()) {
-			Integer value = map.get(key);
-			System.out.println("Key: " + key + ", Value: " + value);
-			list[index] = value;
-			index++;
-		}
-		Arrays.sort(list);
-		System.out.println(Arrays.toString(list));
-		for (int i = list.length - 1; i >= 0; i--) {
-			System.out.println(list[i]);
-			kCnt += list[i];
-		}
-		return answer;
-	}
+		 Map<Integer, Integer> countMap = new HashMap<>();
+	        for (int size : tangerine) {
+	            countMap.put(size, countMap.getOrDefault(size, 0) + 1);
+	        }
+
+	        // 개수를 배열로 변환하고 내림차순으로 정렬한다
+	        Integer[] counts = countMap.values().toArray(new Integer[0]);
+	        Arrays.sort(counts, (a, b) -> b - a);
+
+	        int selected = 0;
+	        int varieties = 0;
+
+	        // 가장 많은 종류의 귤부터 선택하여 k개를 채운다
+	        for (int cnt : counts) {
+	            selected += cnt;
+	            varieties++;
+	            if (selected >= k) {
+	                break;
+	            }
+	        }
+
+	        return varieties;
+	    }
 
 }
