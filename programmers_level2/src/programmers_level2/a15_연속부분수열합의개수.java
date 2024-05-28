190555b0283ece4cca0ee116fba6c324ac3205b5
skswnscjf2@naver.com <skswnscjf2@naver.com>
2024-05-28 23:32:33 +0900

    ready

---
+++ programmers_level2/src/programmers_level2/a15_연속부분수열합의개수.java
@@ -0,0 +1,20 @@
+package programmers_level2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/138476
+public class a15_연속부분수열합의개수 {
+	public static void main(String[] args) {
+
+		int[] elements = {7,9,1,1,4}; 
+		System.out.println(solution(elements)); // 출력:3
+	}
+
+	public static int solution(int[] elements) {
+		Set<Integer> set = new HashSet<>();
+		int answer = 0;
+		return answer;
+	}
+
+}
