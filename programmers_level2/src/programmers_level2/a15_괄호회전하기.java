54c47a620fce9166cc7597257da4a3a73fbc7fbe
junecheolpark <skswnscjf2@naver.com>
2024-06-03 16:57:14 +0900

    괄호 ready

---
+++ programmers_level2/src/programmers_level2/a15_괄호회전하기.java
@@ -0,0 +1,16 @@
+package programmers_level2;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/76502
+public class a15_괄호회전하기 {
+	public static void main(String[] args) {
+
+		System.out.println(solution("[](){}")); // 출력:3
+//		System.out.println(solution("}]()[{")); // 출력:2
+	}
+
+	public static int solution(String s) {
+        int answer = -1;
+        return answer;
+    }
+
+}
