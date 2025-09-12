ff3cdc32d38e33b301d6c0dbf025dd6f2b672205
junecheolpark <skswnscjf2@naver.com>
2025-09-12 23:59:13 +0900

    save

---
+++ programmers_level0/src/programmers_level0/_a68_배열만들기1.java
@@ -0,0 +1,21 @@
+package programmers_level0;
+
+import java.util.Arrays;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/181901?language=java
+
+public class _a68_배열만들기1 {
+	public static void main(String[] arg) {
+		System.out.println(Arrays.toString(solution(10,3)));
+	}
+
+	public static int[] solution(int n, int k) {
+		int size = n / k;
+		int[] arr = new int[size];
+		for (int i = 0; k * i <= n; i++) {
+			arr[i] = k * (i + 1);
+		}
+		
+		return arr;
+	}
+}
