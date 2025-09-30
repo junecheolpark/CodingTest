242c1390b737f630469bc260db97bec66e5904c0
junecheolpark <skswnscjf2@naver.com>
2025-09-30 23:58:53 +0900

    codingTest 0 1

---
+++ programmers_level0/src/programmers_level0/_b161_특수문자출력하기.java
@@ -0,0 +1,12 @@
+package programmers_level0;
+
+/*
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181948
+ * */
+
+public class _b161_특수문자출력하기 {
+	public static void main(String[] arg) {
+		System.out.println("!@#$%^&*(\\'\"<>?:;");
+	}
+
+}
